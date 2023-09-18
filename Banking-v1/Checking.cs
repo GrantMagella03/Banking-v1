ef26f1398b896344724b9991b41de8f55ab7dfd9
Grant Magella <Bitman2003@gmail.com>
2023-09-18 14:47:36 -0400

    reformatting

--- Banking-v1/Checking.cs
+++ Banking-v1/Checking.cs
@@ -1,55 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Banking_v1
-{
-    public class Checking
-    {
+﻿namespace Banking_v1 {
+    public class Checking {
         private Account _Account { get; set; }
         private static int NextCheckNo = 100;
         public int CheckNo { get; set; }
         public int AccountNo => _Account.AccountNo;
         public decimal Balance { get { return _Account.Balance; } }
-        public string Description { get { return _Account.Description; } set { _Account.Description = value; }}
+        public string Description { get { return _Account.Description; } set { _Account.Description = value; } }
         /*public int AccountNo { get { return _Account.AccountNo; }}*/
-        public Checking() 
-        {
+        public Checking() {
             _Account = new Account();
 
         }
 
-        public void print()
-        {
+        public void print() {
             _Account.Print();
         }
-        public bool WriteCheck(decimal Amount)
-        {
+        public bool WriteCheck(decimal Amount) {
             if (!Withdraw(Amount))
                 return false;
             int checkno = NextCheckNo++;
             return true;
             //Console.WriteLine("#"+checkno+" for $"+Amount);
         }
-        public bool Deposit(decimal Amount)
-        {
+        public bool Deposit(decimal Amount) {
             return _Account.Deposit(Amount);
         }
-        public bool Withdraw(decimal Amount)
-        {
+        public bool Withdraw(decimal Amount) {
             return _Account.Withdraw(Amount);
         }
-        public void Transfer(Checking Checking, decimal amount)
-        {
-            if (this.Withdraw(amount))
-            {
+        public void Transfer(Checking Checking, decimal amount) {
+            if (this.Withdraw(amount)) {
                 Checking.Deposit(amount);
                 return;
-            }
-            else
-            {
+            } else {
                 Console.WriteLine("Transfer Failed ");
             }
         }
--- Banking-v1/Savings.cs
+++ Banking-v1/Savings.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Banking_v1
-{
-    public class Savings : Account
-    {
+﻿namespace Banking_v1 {
+    public class Savings : Account {
         public decimal InterestRate { get; set; } = 0.12m;
-        public Savings() : base() 
-        { 
-            
+        public Savings() : base() {
+
         }
-        public decimal CalculateInterestByMonths(int months)
-        {
+        public decimal CalculateInterestByMonths(int months) {
             decimal interest = Balance * (InterestRate / 12) * months;
             Deposit(interest);
             return interest;
